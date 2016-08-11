c3654e4ae909b9c3b4cf9bb41feece0321d7b691
cel <yangwoo@celui-MacBook-Pro.local>
2016-08-11 20:30:26 +0900

    first commit

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ var port = 8000
 app.get("/", function (req, res) {
 	res.sendFile(__dirname + '/index.html')
 })
-
-app.listen(port, function (error) {
+// http.listen(process.env.PORT || 3000, function(){
+//   console.log('listening on', http.address().port);
+// });
+app.listen(process.env.PORT || 3000, function (error) {
 	if (error) {
 		console.error(error)
 	} else {
-		console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+		console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", process.env.PORT || 3000, process.env.PORT || 3000)
 	}
 })
